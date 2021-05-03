427585bef4fa21c7ac2c953ad3a78a34f78b6f86
Dolpheyn <farissufyan99@gmail.com>
2021-05-03 18:49:43 +0800

    Move init to population

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod algo;
+mod phenotype;
+mod population;
 mod stall;
 
-use algo::{GivenStalls, Phenotype};
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use population::Population;
+use stall::GivenStalls;
 use std::fs;
 use std::path::Path;
 
@@ -12,17 +13,12 @@ fn load_stalls_from_file(path: &Path) -> GivenStalls {
     serde_json::from_str(&String::from_utf8_lossy(&data)).unwrap()
 }
 
-fn init(given_stalls: GivenStalls) {
-    let mut rng = thread_rng();
-    let mut genotype: Vec<u8> = given_stalls.iter().map(|s| s.id()).collect();
-    genotype.shuffle(&mut rng);
-    let pheno = Phenotype::new(genotype);
-    println!("{}", pheno.fitness(given_stalls))
-}
-
 fn main() {
     let path = Path::new("stalls.json");
     let given_stalls = load_stalls_from_file(path);
     println!("from file = {:?}", given_stalls);
-    init(given_stalls);
+
+    let pop = Population::init(given_stalls, 10);
+    // let optimizer = Optimizer::init(pop);
+    println!("Population {:?}", pop);
 }
