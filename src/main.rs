a97b994b9049791cf79277168c374c7579f65c41
Dolpheyn <farissufyan99@gmail.com>
2021-05-10 15:29:07 +0800

    main: Print stats after taken final step

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use population::Population;
 use stall::GivenStalls;
 use std::fs;
 use std::path::Path;
+use std::time::{Duration, Instant};
 
 fn load_stalls_from_file(path: &Path) -> GivenStalls {
     let data = fs::read(path).unwrap();
@@ -24,6 +25,8 @@ fn main() {
         .with_max_step(1000);
 
     println!("{:?}", optimizer);
+    let start_time = Instant::now();
+
     loop {
         let step = optimizer.step();
 
@@ -39,6 +42,8 @@ fn main() {
                 best: _best,
                 best_as_stalls: _best_as_stalls,
             } => {
+                let duration = start_time.elapsed();
+
                 println!(
                     "Reached final step. Best stall configuration with {} fitness score:\n",
                     _best.fitness(&given_stalls)
@@ -47,6 +52,9 @@ fn main() {
                 for s in _best_as_stalls {
                     println!("{} : c{}", s.name, s.category);
                 }
+
+                println!("\nSolution found in {} steps", optimizer.num_step_taken());
+                println!("Time elapsed: {:?}", duration);
                 break;
             }
         }
