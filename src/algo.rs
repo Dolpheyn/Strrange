996c464e13278545055fbd4bb99283e856dfe334
Dolpheyn <farissufyan99@gmail.com>
2021-05-10 14:48:29 +0800

    algo: Rename iter-> step, add set methods

--- src/algo.rs
+++ src/algo.rs
@@ -3,22 +3,49 @@ use crate::population::Population;
 use crate::stall::Stall;
 use rand::{thread_rng, Rng};
 
-struct Optimizer {
+#[derive(Debug)]
+pub struct Optimizer {
     the_population: Population,
     crossover_rate: f32,
     mutation_rate: f32,
-    cur_iter: u32,
-    max_iter: u32,
+    cur_step: u32,
+    max_step: u32,
 }
 
-enum Step {
+#[derive(Debug)]
+pub enum Step {
     Intermediate(Phenotype, Vec<Stall>, Vec<Phenotype>),
     Final(Phenotype, Vec<Stall>),
 }
 
 impl Optimizer {
-    fn reached_max_iter(&self) -> bool {
-        self.cur_iter == self.max_iter
+    pub fn new(population: Population) -> Optimizer {
+        Optimizer {
+            the_population: population,
+            crossover_rate: 0.0,
+            mutation_rate: 0.0,
+            cur_step: 0,
+            max_step: 0,
+        }
+    }
+
+    pub fn with_crossover_rate(mut self, crossover_rate: f32) -> Self {
+        self.crossover_rate = crossover_rate;
+        self
+    }
+
+    pub fn with_mutation_rate(mut self, mutation_rate: f32) -> Self {
+        self.mutation_rate = mutation_rate;
+        self
+    }
+
+    pub fn with_max_step(mut self, max_step: u32) -> Self {
+        self.max_step = max_step;
+        self
+    }
+
+    fn reached_max_step(&self) -> bool {
+        self.cur_step == self.max_step
     }
 
     fn crossover(&self, best: &Phenotype, second_best: &Phenotype) -> Vec<u8> {
@@ -83,13 +110,13 @@ impl Optimizer {
         let given_stalls = &self.the_population.given_stalls;
         let mut population = self.the_population.population.clone();
 
+        // Selection
         population.sort_by(|a, b| a.fitness(given_stalls).cmp(&b.fitness(given_stalls))); // Sort by fitness, lower = better.
-
         let p1 = population[0].clone();
         let p2 = population[1].clone();
 
         // Termination Case
-        if p1.fitness(given_stalls) == 0 || self.reached_max_iter() {
+        if p1.fitness(given_stalls) == 0 || self.reached_max_step() {
             let best = p1;
             return Step::Final(best.clone(), best.as_stalls(given_stalls));
         }
